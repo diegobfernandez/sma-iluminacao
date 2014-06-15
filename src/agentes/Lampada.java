a8b268e87948546b4cbfbf98c1a8d5ec6633d073
Cristian <cristian@cristian-mint.(none)>
2014-06-15 16:26:33 -0300

    Adicionado comportamento para acender Lampada

--- src/agentes/Lampada.java
+++ src/agentes/Lampada.java
@@ -1,9 +1,12 @@
 package agentes;
 
+import comportamentos.Acender;
 import jade.core.Agent;
 
 public class Lampada extends Agent {
     @Override
     protected void setup() {
+        //Fazer a verificação de quando esse comportamento deve ser disparado;
+        addBehaviour(new Acender(this));
     }
 }
---
+++ src/comportamentos/Acender.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comportamentos;
+
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+
+/**
+ *
+ * @author cristian
+ */
+public class Acender extends Behaviour{
+
+    public Acender(Agent lampada) {
+        super(lampada);
+    }
+
+    @Override
+    public void action() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean done() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
